03cf51cff7572ea5a31505db33e65c44ef3416fa
William Sedlacek <wsedlacekc@gmail.com>
2019-10-01 13:05:53 -0500

    Add in Followers and Error Handling
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/app/app.component.tsx
+++ src/app/app.component.tsx
@@ -1,34 +1,64 @@
 import React from 'react';
 import axios from 'axios';
 
-import { UserCard } from './components/user-card/user-card.component';
+import { Typography } from '@material-ui/core';
+
 import { User } from './models/User';
+import { UserCard } from './components/user-card/user-card.component';
 
 export type AppProps = {};
 export type AppState = {
   user: User | null;
+  followers: User[];
+  error: boolean;
 };
 
 class App extends React.Component<AppProps, AppState> {
   constructor(props: AppProps) {
     super(props);
 
-    this.state = { user: null };
+    this.state = {
+      user: null,
+      followers: [],
+      error: false,
+    };
 
     const user = 'wSedlacek';
+
     axios
       .get<User>(`https://api.github.com/users/${user}`)
-      .then((res) => this.setState({ user: res.data }));
+      .then((res) => this.setState({ user: res.data }))
+      .catch(() => this.setState({ error: true }));
+  }
+
+  componentDidUpdate() {
+    const { user, followers } = this.state;
+    if (user && followers.length === 0)
+      axios
+        .get<User[]>(user.followers_url)
+        .then((res) => this.setState({ followers: res.data }))
+        .catch(() => this.setState({ error: true }));
   }
 
   render() {
-    const { user } = this.state;
+    const { user, followers, error } = this.state;
 
-    if (!user) return <div>Loading...</div>;
+    if (error)
+      return (
+        <Typography variant='h3'>
+          Error - Could not load data, this is typically because of the GitHub API limit.
+        </Typography>
+      );
+    if (!user) return <Typography variant='h3'>Loading...</Typography>;
 
     return (
       <div>
+        <Typography variant='h1'>Me</Typography>
         <UserCard user={user} />
+        <Typography variant='h1'>Followers</Typography>
+        {followers.map((follower) => (
+          <UserCard user={follower} />
+        ))}
       </div>
     );
   }
