828c03965f78ce18040d104251e54a4fdefc9aa6
William Sedlacek <wsedlacekc@gmail.com>
2019-10-01 11:35:51 -0500

    Create User Card
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/app/components/user-card/user-card.component.tsx
+++ src/app/components/user-card/user-card.component.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Card, CardContent, Typography } from '@material-ui/core';
 
-class UserCard extends React.Component {
+import { User } from '../../models/User';
+
+export type UserCardProps = {
+  user: User;
+};
+
+export type UserCardState = {};
+
+class UserCard extends React.Component<UserCardProps, UserCardState> {
   render() {
-    return <div></div>;
+    const { name, login } = this.props.user;
+
+    return (
+      <Card>
+        <CardContent>
+          <Typography variant='h3'>{name || login}</Typography>
+        </CardContent>
+      </Card>
+    );
   }
 }
 
